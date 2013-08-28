b0a6563bb862ccb3b797513241e815bbe2cf994b
Zygonie <github@zygonie.com>
2013-08-27 22:47:44 -0400

    Posting profile bug has been fixed.

--- routes/routes.js
+++ routes/routes.js
@@ -9,21 +9,6 @@ exports.home = function(req, res) {
 	res.redirect('/');
 	};
 
-exports.getprofile = function(req, res) {
-	var user = req.user;
-	db.profileModel.findOne({ username: user.username }, function(err, profile) {
-	    if (err) { 
-	    	res.redirect('/'); 
-	    }	    
-	    res.render("profile", {
-	  	  title: 'Profile',
-	  	  id: 'profile',
-	  	  profile: profile,
-	  	  user: req.user
-	    });
-	});  
-};
-
 exports.getlogin = function(req, res) {
   res.render('login');
 };
@@ -63,7 +48,7 @@ app.post('/login',
   
 // POST /login
 //   This is an alternative implementation that uses a custom callback to
-//   acheive the same functionality.
+//   achieve the same functionality.
 exports.postlogin = function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) { return next(err); }
@@ -73,14 +58,17 @@ exports.postlogin = function(req, res, next) {
     }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
+      console.log('User ' + user.username + ' has logged in.');
       return res.redirect('/');
     });
   })(req, res, next);
 };
 
 exports.logout = function(req, res) {
-  req.logout();
-  res.redirect('/');
+	var name = req.user.username;
+	req.logout();
+	console.log('User ' + name + ' has logged out.');
+	res.redirect('/');
 };
 
 exports.getregister = function(req, res) {
@@ -95,13 +83,13 @@ exports.postregister = function(req, res) {
 			console.log(err);
 	    } 
 		else {
-			console.log('user: ' + user.username + " saved.");
+			console.log('New user: ' + user.username + " has register.");
 			profile.save(function(err) {
     			if(err) {
     				console.log(err);
     		    } 
     			else {
-    				console.log('profile for user: ' + profile.username + " saved.");
+    				console.log('Profile for user: ' + profile.username + " saved.");
     				req.logIn(user, function(err) {
     				      if (err) { return next(err); }
     				      return res.redirect('/');
@@ -112,26 +100,32 @@ exports.postregister = function(req, res) {
 	});
 };
 
-exports.postprofile = function(req, res, next) {	
-	passport.authenticate('local', function(err, user, info) {
-	    if (err) { return next(err); }
-	    if (!user) {
-	      req.session.messages =  [info.message];
-	      console.log(info.message);
-	      return res.redirect('/login');
-	    }
-	      var profile = new db.userModel({ username: req.body.username, email: req.body.email, 
-              age: req.body.username, address: req.body.username, 
-              city: req.body.username, zipcode: req.body.username });
-	      profile.save(function(err) {
-	    	  if(err) {
-	    		  console.log(err);
-	          }
-	    	  else {
-	    		  console.log('profile for user: ' + profile.username + " updated.");
-	    	  }
-	      });
-	      return res.redirect('/');
-	})(req, res, next);
+exports.getprofile = function(req, res) {
+	var user = req.user;
+	db.profileModel.findOne({ username: user.username }, function(err, profile) {
+	    if (err) { 
+	    	res.redirect('/'); 
+	    }	    
+	    res.render("profile", {
+	  	  title: 'Profile',
+	  	  id: 'profile',
+	  	  profile: profile,
+	  	  user: req.user
+	    });
+	});  
+};
+
+exports.postprofile = function(req, res) {
+	var profile = req.body;
+	db.profileModel.update({'username':profile.username},profile,{safe:true}, function(err, result){
+		if(err) {
+			console.log('Error updating profile. ' + err);
+			res.redirect('/');
+		}
+		else{
+			console.log('' + result + ' profile updated for user: ' + profile.username);
+			res.redirect('/');
+		}			
+	});
 };
 

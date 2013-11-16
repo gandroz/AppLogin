fbd1b9318b9d53fabb7e5c4243107fdcbd6c8c8e
Zygonie <github@zygonie.com>
2013-11-16 16:12:34 -0500

    Spinner added to wait for data loaded before bootstrapping

--- public/js/controllers/backlogCtrl.js
+++ public/js/controllers/backlogCtrl.js
@@ -1,9 +1,32 @@
-function backlogCtrl($scope, $log, $location, $filter, $window, $element, $route,  logEntries) {	
+function backlogCtrl($scope, $log, $location, $filter, $window, $element, $route, $timeout, logEntries) {	
 	    
 	    $scope.data = {};
 	    $scope.isCollapsed = {};
 	    $scope.buttonText = 'Create';
 	    $scope.query="";
+	    $scope.loaded = false;
+	    
+	  //Spinner
+		var opts = {
+				  lines: 13, // The number of lines to draw
+				  length: 20, // The length of each line
+				  width: 10, // The line thickness
+				  radius: 30, // The radius of the inner circle
+				  corners: 1, // Corner roundness (0..1)
+				  rotate: 0, // The rotation offset
+				  direction: 1, // 1: clockwise, -1: counterclockwise
+				  color: '#000', // #rgb or #rrggbb or array of colors
+				  speed: 1, // Rounds per second
+				  trail: 60, // Afterglow percentage
+				  shadow: false, // Whether to render a shadow
+				  hwaccel: false, // Whether to use hardware acceleration
+				  className: 'spinner', // The CSS class to assign to the spinner
+				  zIndex: 2e9, // The z-index (defaults to 2000000000)
+				  top: 'auto', // Top position relative to parent in px
+				  left: 'auto' // Left position relative to parent in px
+				};
+		var target = document.getElementById('spinner');
+		var spinner = new Spinner(opts).spin(target);
 	    
 	    $scope.searchNotDone = function(item) {
 	    	return  $scope.search(item, false);
@@ -36,9 +59,11 @@ function backlogCtrl($scope, $log, $location, $filter, $window, $element, $route
 	    }
 	    
 		$scope.init = function(){
-			$scope.isCollapsed = true;
+			$scope.isCollapsed = true;			
 			logEntries.query(function(res){
 				$scope.data.entries = res;
+				$scope.loaded = true;
+				spinner.stop();
 				for(var idx in $scope.data.entries)
 				{
 					var item = $scope.data.entries[idx];
--- public/js/controllers/jobOfferCtrl.js
+++ public/js/controllers/jobOfferCtrl.js
@@ -1,5 +1,6 @@
 function myJobOfferListCtrl($scope, $log, $routeParams, $location, $window, $route, $modal, Api) {
 	$scope.data = {};
+	$scope.loaded = false;
 	
 	$scope.initProfile = function() {
 		Api.Profile.query(function(res){
@@ -37,6 +38,31 @@ function myJobOfferListCtrl($scope, $log, $routeParams, $location, $window, $rou
 	};
 	
 	$scope.initDashboard = function(){
+		var jobLoaded = false,
+		    profileLoaded = false;
+		
+		//Spinner
+		var opts = {
+				  lines: 13, // The number of lines to draw
+				  length: 20, // The length of each line
+				  width: 10, // The line thickness
+				  radius: 30, // The radius of the inner circle
+				  corners: 1, // Corner roundness (0..1)
+				  rotate: 0, // The rotation offset
+				  direction: 1, // 1: clockwise, -1: counterclockwise
+				  color: '#000', // #rgb or #rrggbb or array of colors
+				  speed: 1, // Rounds per second
+				  trail: 60, // Afterglow percentage
+				  shadow: false, // Whether to render a shadow
+				  hwaccel: false, // Whether to use hardware acceleration
+				  className: 'spinner', // The CSS class to assign to the spinner
+				  zIndex: 2e9, // The z-index (defaults to 2000000000)
+				  top: 'auto', // Top position relative to parent in px
+				  left: 'auto' // Left position relative to parent in px
+				};
+		var target = document.getElementById('spinner');
+		var spinner = new Spinner(opts).spin(target);
+		
 		$scope.filterOptions = {
 		        filterText: "",
 		        useExternalFilter: true
@@ -94,10 +120,16 @@ function myJobOfferListCtrl($scope, $log, $routeParams, $location, $window, $rou
 				
 		Api.LastSevenJobs.query(function(res){
 		   $scope.data.lastJobs = res;
+		   jobLoaded = true;
+		   $scope.loaded = jobLoaded && profileLoaded;
+		   if($scope.loaded) spinner.stop();
 	    });
 		
 		Api.Profile.query(function(res){
 			$scope.data.profile = res;
+			profileLoaded = true;
+			$scope.loaded = jobLoaded && profileLoaded;
+			if($scope.loaded) spinner.stop();
 		});
 	};
 	
